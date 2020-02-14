acbd83bfde5ac3767a93c9ff6847352630cbb86f
sachin mohite <sachinmohite@tersesoft.com>
2020-02-14 08:43:49 +0530

    star printing and else if program

--- star_input.go
+++ star_input.go
@@ -1,11 +1,13 @@
 package main
 
-import ("fmt")
+import ("fmt"	)
 
 func main(){
 
-	var  i,j,level int = 0,0,40
+	var i,j,level int = 0,0,40
 
+	fmt.Printf("Enter number of levels:")
+	fmt.Scan(&level)
 
 	for i=0;i<level;i++{
 		j = i + 1
